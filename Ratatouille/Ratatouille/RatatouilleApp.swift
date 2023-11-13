6bbd6f6522cf81e1a5a782d876bea39bef3bd589
Øystein Engell <89580365+OysteinEngell@users.noreply.github.com>
2023-11-13 14:17:22 +0100

    Init project

---
+++ Ratatouille/Ratatouille/RatatouilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatatouilleApp.swift
+//  Ratatouille
+//
+//  Created by Øystein Engell on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RatatouilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
